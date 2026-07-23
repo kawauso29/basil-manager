679ca3f91876d6b36495f4cc5717e83665fcb908
kawauso29 <may29kh@gmail.com>
2026-07-23 15:26:47 +0900

    admin basic認証追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,8 @@
 class Admin::BaseController < ApplicationController
   layout "admin"
   include Admin::FlashMessages
+  http_basic_authenticate_with(
+    name: Rails.application.credentials.dig(:admin, :user),
+    password: Rails.application.credentials.dig(:admin, :password)
+  )
 end
